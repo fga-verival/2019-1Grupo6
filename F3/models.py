52b1f9f30062804ac0fe4774d765638485e62cb6
Mateus Nóbrega <mateusn@outlook.com>
2019-06-09 16:12:41 -0300

    Adding html to the listing pattern

--- F3/models.py
+++ F3/models.py
@@ -3,7 +3,7 @@ from .constants import *
 
 class TransactionalFunction(models.Model):
     name = models.CharField(verbose_name="Nome", max_length=200,blank=False)
-    f_type = models.IntegerField(verbose_name="Tipo de funcionalidade", choices=F_TYPE_CHOICES,blank=False)
+    f_type = models.IntegerField(verbose_name="Tipo de funcionalidade", choices=F_TYPE_CHOICES,blank=False,default=0)
     qt_ALR = models.IntegerField(verbose_name="Quantidade de ALR", blank=False)
     qt_DER = models.IntegerField(verbose_name="Quantidade de DER", blank=False)
     function_points = models.IntegerField(verbose_name="Pontos de Função")
--- F3/views.py
+++ F3/views.py
@@ -7,7 +7,7 @@ from .models import TransactionalFunction
 class TransactionalFunctionCreate(CreateView):
     model = TransactionalFunction
     fields = ['name', 'f_type', 'qt_ALR', 'qt_DER', 'function_points', 'counter_name', 'complexity']
-    success_url = reverse_lazy('admin/')
+    success_url = reverse_lazy('')
 
 
 class TransactionalFunctionList(ListView):
