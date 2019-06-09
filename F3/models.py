3bfab45eee532736b5d08fe5f979a7883236ffd1
Mateus Nóbrega <mateusn@outlook.com>
2019-06-09 01:03:12 -0300

    Starting definition of the application's models

--- F3/models.py
+++ F3/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from .constants import *
 
-# Create your models here.
 class TransactionalFunction(models.Model):
-    pass
+    name = models.CharField(verbose_name="Name", max_length=200)
+    f_type = models.IntegerField(verbose_name="Functionality type", choices=F_TYPE_CHOICES)
+    qt_ALR = models.IntegerField()
+    qt_DER = models.IntegerField()
+    function_points = models.IntegerField()
+    counter_name = models.CharField(max_length=200)
+    date = models.DateField(auto_now=True)
+    # TODO: transactional function complexity
